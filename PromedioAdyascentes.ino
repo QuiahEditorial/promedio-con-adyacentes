eb4a0e039f255d2b0e077ada68cb25ed748343a9
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-26 09:40:27 -0600

    Update PromedioAdyascentes.ino

--- PromedioAdyascentes.ino
+++ PromedioAdyascentes.ino
@@ -1,9 +1,27 @@
+int n = 9;           // n+1
+int Pm[n];           // subconjunto de n datos
+int m = n-1;         // contador
+int I;               // intensidad
+int Ip;              // intensidad promedio
 
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+I = analogRead(A0);
+Serial.print(I);
+PMD();
+Serial.print(Ip);
+}
 
 void PMD ()
 {
-  for ( int j=0; j<=k; j++) { Prm[j] = Prm[j+1]; }
-  Prm[k] = I ;
-  Ipm = 0;
-  for ( int j=0; j<=k; j++ ) { Ipm = Ipm + Prm[k]; } Ipm = Ipm / K;
+  for ( int i=0; i<=m-1; i++ ) { Prm[i] = Prm[i+1]; }
+  Pm[m] = I ;
+  Ip    = 0;
+  for ( int i=0; i<= m;  i++ ) { Ip = Ip + Pm[m]; } 
+  Ip = Ip / n;
 }
